b50855ffe184cc94097675c6c811c66eb1039270
thandiweportia <111557606+thandiweportia@users.noreply.github.com>
2023-06-24 18:29:50 +0200

    Adding features for explore screen

---
+++ app/src/main/java/com/project/gradapp/exploreScreen.java
@@ -0,0 +1,14 @@
+package com.project.gradapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class exploreScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_screen);
+    }
+}
