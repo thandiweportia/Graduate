131f38e44c0209e6c2e57c57fc43de437a213f5a
thandiweportia <111557606+thandiweportia@users.noreply.github.com>
2023-06-25 20:31:31 +0200

    Adding backendcode for main activity

--- app/src/main/java/com/project/gradapp/MainActivity.java
+++ app/src/main/java/com/project/gradapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.project.gradapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+@SuppressWarnings("ALL")
 public class MainActivity extends AppCompatActivity {
+    private Button signInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signInBtn = (Button) findViewById(R.id.signInBtn);
+        signInBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openComplete();
+            }
+        });
+    }
+
+    public void openComplete() {
+        Intent intent = new Intent(this, Complete.class);
+        startActivity(intent);
     }
 }
