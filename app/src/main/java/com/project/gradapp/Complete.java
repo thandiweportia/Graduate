decfeb2e25d5970ae04342e6dcbf022f05fe089f
thandiweportia <111557606+thandiweportia@users.noreply.github.com>
2023-06-27 09:05:10 +0200

    Adding java code for backend

--- app/src/main/java/com/project/gradapp/Complete.java
+++ app/src/main/java/com/project/gradapp/Complete.java
@@ -1,14 +1,53 @@
 package com.project.gradapp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Complete extends AppCompatActivity {
+    EditText username;
+    EditText email;
+    Button loginButton;
+
+    String[] institution = { "University of Johannesburg", "Wits",
+            "University of Pretoria", "University of Cape Town",
+            "University of Western Cape", "University of South Africa" };
+    String[] streams = { "Web", "Data structures",
+            "Mobile", "Desktop Application",
+            "DSA with java", "UI/UX" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_complete);
+
+        Spinner institution = findViewById(R.id.institutionSpinner);
+        Spinner streams = findViewById(R.id.streamSpinner);
+
+        //spin.setOnItemSelectedListener(this);
+
+        // Create the instance of ArrayAdapter
+        // having the list of courses
+
+
+
+        username = findViewById(R.id.nameEdit);
+        email = findViewById(R.id.emailEdit);
+        loginButton = findViewById(R.id.continueBtn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && email.getText().toString().equals("1234")) {
+                    Toast.makeText(Complete.this, "Login successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Complete.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/project/gradapp/profilePicture.java
+++ app/src/main/java/com/project/gradapp/profilePicture.java
@@ -1,8 +1,10 @@
 package com.project.gradapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class profilePicture extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class profilePicture extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_picture);
+
+        Button finishBtn = (Button) findViewById(R.id.finishBtn);
+        finishBtn.setOnClickListener(v -> openexploreScreen());
+    }
+
+    public void openexploreScreen() {
+        Intent intent = new Intent(this, exploreScreen.class);
+        startActivity(intent);
     }
 }
